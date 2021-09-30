1294e9f01ba466bf346cfc2014d4b9bcfabca2dc
Vidiborets <45356315+Vidiborets@users.noreply.github.com>
2021-09-30 20:59:32 +0000

    Deploying to gh-pages from @ Vidiborets/goit-js-hw-13-image-finder@36a856a4304bceb34574f07ba6056e528c2eb4ae 🚀

--- src/js/refsPictures.js
+++
@@ -1,7 +0,0 @@
-const refs = {
-    cardContainer: document.querySelector('.gallery'),
-    searchForm: document.querySelector('.js-search-form'),
-    inputForm: document.querySelector('.form-control'),
-    loadMoreBtn: document.querySelector('.show-more'),
-}
-export default refs
